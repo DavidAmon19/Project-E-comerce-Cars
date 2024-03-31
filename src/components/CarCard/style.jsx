c42ea03e5b135e600eb8b4f5ca94bd4e39dda9f6
David Amorim <davi.amorin2@gmail.com>
2024-03-31 18:55:26 -0300

    (CarModal)=> Criação de modal com mais informações e função para abri-la ao clicar em mais

--- src/components/CarCard/style.jsx
+++ src/components/CarCard/style.jsx
@@ -7,7 +7,14 @@ export const DivContent = styled.div `
     justfy-content:center;
     flex-wrap: wrap;
     gap:2rem;
-    height:100vh;
+    height:100%;
     width: 100%;
     margin: 4rem 1rem;
 `;
+
+export const DivCard = styled.div `
+    display:flex;
+    align-items:center;
+    margin:0 auto;
+    gap:2rem;
+    `;
