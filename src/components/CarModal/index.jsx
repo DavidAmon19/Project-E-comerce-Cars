120f4cb04212d48a1b4884b1a2d3f38257bdce2b
David Amorim <davi.amorin2@gmail.com>
2024-04-01 17:10:32 -0300

    (selectedCar)=> Função para seleção de carro pro modal

--- src/components/CarModal/index.jsx
+++ src/components/CarModal/index.jsx
@@ -1,70 +1,67 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { DivDetails } from "./style";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(10),
   },
-  '& .MuiDialogActions-root': {
-    padding: theme.spacing(1),
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(5),
   },
 }));
 
-export default function CarModal({ open, handleClose }) { // Recebe as props open e handleClose
+export default function CarModal({ open, handleClose, selectedCar }) {
+  if (!selectedCar) {
+    return null;
+    0;
+  }
+
+  const { modelo, description, marca, preco, ano } = selectedCar;
+
   return (
-    <React.Fragment>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
+    <BootstrapDialog
+      onClose={handleClose}
+      aria-labelledby="customized-dialog-title"
+      open={open}
+    >
+      <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+        {modelo}
+      </DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={handleClose}
+        sx={{
+          position: "absolute",
+          right: 8,
+          top: 8,
+          color: (theme) => theme.palette.grey[500],
+        }}
       >
-        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Modal title
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-            ullamcorper nulla non metus auctor fringilla.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Save changes
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
-    </React.Fragment>
+        <CloseIcon />
+      </IconButton>
+      <DialogContent dividers>
+        <Typography gutterBottom>{description}</Typography>
+      </DialogContent>
+      <DialogContent dividers>
+        <Typography gutterBottom>
+          <DivDetails>
+            {preco}
+            {marca}
+            {modelo}
+            {ano}
+          </DivDetails>
+        </Typography>
+      </DialogContent>
+    </BootstrapDialog>
   );
 }
-
-
---
+++ src/components/CarModal/style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DivDetails = styled.div`
+
+display flex;
+flex-direction column;
+justify-content: center;
+align-items:center;
+border-bottom: 1px solid #ccc;
+`;
